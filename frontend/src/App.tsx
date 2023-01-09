43ff7420d30ea9a2c4767ff7a06f3f0150cd53ec
Muzammil <muzammilfive@gmail.com>
2023-01-09 02:51:34 +0000

    Refactor code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 import {NavBar} from "./nav-bar/nav-bar";
 import {Authentication} from "./authentication/Authentication";
-import {UserDashboard} from "./user-dashboard/user-dashboard";
 import {AdminDashboard} from "./admin-dashboard/admin-dashboard";
+import {CustomerDashboard} from "./customer-dashboard/customer-dashboard";
 
 function App() {
     const role = sessionStorage.getItem("role");
     return (
         <div className="App">
             <NavBar role={role || undefined}/>
-            {role === null || role === undefined ? <Authentication/> : (role === "customer" ? <UserDashboard/> :
+            {role === null || role === undefined ? <Authentication/> : (role === "customer" ? <CustomerDashboard/> :
                 <AdminDashboard/>)}
         </div>
     );
